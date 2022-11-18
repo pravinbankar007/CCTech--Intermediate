1bd8be26f9b3ade978056e40d44dbf35dbe6da0f
Pravin Bankar <99469166+pravinbankar007@users.noreply.github.com>
2022-11-18 22:30:00 +0530

    Add files via upload

--- String Anagrams/StringAnagrams.cpp
+++ String Anagrams/StringAnagrams.cpp
@@ -1,1 +1,35 @@
 
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+void printAnagramPermutations(string str)
+{
+    sort(str.begin(), str.end());
+
+    do
+    {
+        cout << str << endl;
+    } while (next_permutation(str.begin(), str.end()));
+}
+
+int main()
+{
+    string str;
+
+    cout << "Enter a string : ";
+
+    getline(cin, str);
+
+    if (str.length() == 0)
+    {
+        cout << "Given string is empty";
+    }
+    else
+        cout << "Potential Anagrams of given string are : \n";
+    printAnagramPermutations(str);
+
+    return 0;
+}
