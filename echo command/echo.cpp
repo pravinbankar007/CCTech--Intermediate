7b1a14a2b9b130c003b98dc86de13cbd2fdb03e4
Pravin Bankar <99469166+pravinbankar007@users.noreply.github.com>
2022-11-18 22:59:50 +0530

    Add files via upload

--- echo command/echo.cpp
+++ echo command/echo.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+int main(int argc, char const *argv[])
+{
+
+    if (argc == 1)
+    {
+        cout << "\n";
+    }
+
+    else
+    {
+
+        for (int i = 1; i < argc; i++)
+        {
+            cout << argv[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
