ed2f95b64625210bbb34190d31cc4fd828307868
Pravin Bankar <99469166+pravinbankar007@users.noreply.github.com>
2022-11-25 22:10:04 +0530

    Add files via upload

--- SquareRoot/SquareRoot.cpp
+++ SquareRoot/SquareRoot.cpp
@@ -1,1 +1,44 @@
+#include <iostream>
+#include <cmath>
 
+using namespace std;
+
+class SquareRoot
+{
+public:
+    double num;
+
+    void getSquareRoot()
+    {
+
+        cout << "Enter a number : ";
+
+        cin >> num;
+
+        if (num >= 0)
+        {
+            cout << "Square root of given number : " << sqrt(num);
+        }
+
+        else
+        {
+            throw runtime_error("Invalid input : can't find the square root of negative number ");
+        }
+    }
+};
+
+int main()
+{
+
+    SquareRoot sq1;
+    try
+    {
+        sq1.getSquareRoot();
+    }
+    catch (runtime_error &excep)
+    {
+        cerr << excep.what();
+    }
+
+    return 0;
+}
